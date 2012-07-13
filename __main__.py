915263b73ede5d09d0fa015ca32dfeb72092e463
ooz <oliverzscheyge@gmail.com>
2012-07-13 21:46:51 +0200

    Contest applicable main

--- __main__.py
+++ __main__.py
@@ -7,21 +7,20 @@ __version__ = "0.1.0"
 
 
 
-import sys
-from   optparse import OptionParser
-
-usage  = "Usage: %prog [options] file(s)" 
-parser = OptionParser(usage = usage)
-
-parser.add_option("-v", "--verbose",
-                  action="store_true", dest="verbose", default=False,
-                  help="Display the solving process live on stdout (each step is delayed to be easier followed by a human). Last line printed is the command sequence.")
-
+# import sys
+# from   optparse import OptionParser
+# 
+# usage  = "Usage: %prog [options] file(s)" 
+# parser = OptionParser(usage = usage)
+# 
+# parser.add_option("-v", "--verbose",
+#                   action="store_true", dest="verbose", default=False,
+#                   help="Display the solving process live on stdout (each step is delayed to be easier followed by a human). Last line printed is the command sequence.")
 
 
 """ MAIN """
 if __name__ == "__main__":
-    (options, args) = parser.parse_args()
+#    (options, args) = parser.parse_args()
 
     from minemap import Map
     from mapload import MapLoader
@@ -30,4 +29,8 @@ if __name__ == "__main__":
     m  = ml.mapFromStdin()
     
     if (m != None):
-        m.printAll()
+        from foobot import Robot
+        bot = Robot(m)
+        bot.solve()
+        print bot.cmds
+
