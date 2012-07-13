ce87442401dd8efcea2afc1448d35ef7e0a1791e
ooz <oliverzscheyge@gmail.com>
2012-07-14 01:29:29 +0200

    Add SIGINT handler

--- __main__.py
+++ __main__.py
@@ -5,9 +5,8 @@ __author__  = "Oliver Zscheyge"
 __email__   = "oliverzscheyge@gmail.com"
 __version__ = "0.1.0"
 
-
-
-# import sys
+import sys
+import signal
 # from   optparse import OptionParser
 # 
 # usage  = "Usage: %prog [options] file(s)" 
@@ -17,10 +16,18 @@ __version__ = "0.1.0"
 #                   action="store_true", dest="verbose", default=False,
 #                   help="Display the solving process live on stdout (each step is delayed to be easier followed by a human). Last line printed is the command sequence.")
 
+BOT = None
+def sigintHandler(signal, frame):
+    if (BOT != None):
+        sys.stdout.flush()
+        print BOT.getCommands()
+        sys.exit(0)
 
 """ MAIN """
 if __name__ == "__main__":
 #    (options, args) = parser.parse_args()
+
+    signal.signal(signal.SIGINT, sigintHandler)
 
     from minemap import Map
     from mapload import MapLoader
@@ -30,7 +37,7 @@ if __name__ == "__main__":
     
     if (m != None):
         from foobot import Robot
-        bot = Robot(m)
-        bot.solve()
-        print bot.cmds
+        BOT = Robot(m)
+        BOT.solve()
+        print BOT.getCommands()
 
--- test.py
+++ test.py
@@ -5,33 +5,40 @@ __author__  = "Oliver Zscheyge"
 __email__   = "oliverzscheyge@gmail.com"
 __version__ = "0.1.0"
 
-
-
 import sys
-from   optparse import OptionParser
+import signal
+# from   optparse import OptionParser
+# 
+# usage  = "Usage: %prog [options] file(s)" 
+# parser = OptionParser(usage = usage)
+# 
+# parser.add_option("-v", "--verbose",
+#                   action="store_true", dest="verbose", default=False,
+#                   help="Display the solving process live on stdout (each step is delayed to be easier followed by a human). Last line printed is the command sequence.")
 
-usage  = "Usage: %prog [options] file(s)" 
-parser = OptionParser(usage = usage)
-
-parser.add_option("-v", "--verbose",
-                  action="store_true", dest="verbose", default=False,
-                  help="Display the solving process live on stdout (each step is delayed to be easier followed by a human). Last line printed is the command sequence.")
-
-
+BOT = None
+def sigintHandler(signal, frame):
+    if (BOT != None):
+        sys.stdout.flush()
+        print BOT.getCommands()
+        sys.exit(0)
 
 """ MAIN """
 if __name__ == "__main__":
-    (options, args) = parser.parse_args()
+#    (options, args) = parser.parse_args()
+
+    signal.signal(signal.SIGINT, sigintHandler)
 
     from minemap import Map
     from mapload import MapLoader
 
     ml = MapLoader()
     m  = ml.mapFromFile("maps/contest1.map")
-    
+
     if (m != None):
         m.printAll()
 
         from foobot import Robot
-        bot = Robot(m)
-        bot.solveVisual()
+        BOT = Robot(m)
+        BOT.solveVisual()
+        print BOT.getCommands()
