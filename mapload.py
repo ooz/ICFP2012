1b6fe8dc57dcaef0116ed55a50dc4e6ad69dd24b
ooz <oliverzscheyge@gmail.com>
2012-07-13 20:29:21 +0200

    Add line padding

--- mapload.py
+++ mapload.py
@@ -6,14 +6,20 @@ class MapLoader:
     def __init__(self):
         pass
 
+    def __padLine(self, line, maxLength):
+        if len(line) < maxLength:
+            return self.__padLine(line + " ", maxLength)
+        return line
+
     def __mapFromInputLines(self, lines):
         lines.reverse()
+        maxLength = max(map(lambda l: len(l), lines))
+        lines = map(lambda l: self.__padLine(l, maxLength), lines)
         return Map(lines)
 
     def mapFromStdin(self):
         lines = sys.stdin.readlines()
         lines = map(lambda l: l.strip(), lines)
-        print lines
         return self.__mapFromInputLines(lines)
 
     def mapFromFile(self, path):
